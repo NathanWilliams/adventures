ebdb66be8a460ff69bd7559e27a358c870023446
Nathan Williams <nathan.andrew.williams@gmail.com>
2011-11-07 07:55:46 +1100

    Still getting the hang of git, still having trouble commiting files every now & then

--- experiments/curses/multi_win.py
+++ experiments/curses/multi_win.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
 
 import curses
-
+import curses.panel
 
 def main(screen):
     curses.start_color()
     screen.border(0)
     screen.addstr(0,0, 'Hello world')
-    
+
+    sub_win = curses.newwin(10, 20, 30, 20)
+    sub_win.border(0)
+    sub_win.addstr(3,4, 'blah')
+    screen_panel = curses.panel.new_panel(screen)
+    sw_panel = curses.panel.new_panel(sub_win)
+    sw_panel.top()
+    curses.panel.update_panels()
+    curses.doupdate()
+    sub_win.getch()
+    sw_panel.hide()
+    screen.getch()
+    curses.endwin()
 
 
 if __name__ == '__main__':

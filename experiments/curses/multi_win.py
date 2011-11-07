b2ecb8886a9c09fb8b52d0fa3a769e95574efb7b
Nathan Williams <nathan.andrew.williams@gmail.com>
2011-11-07 12:37:14 +1100

    Rename multi so I could do an acutal multi window version

--- experiments/curses/multi_win.py
+++ experiments/curses/multi_win.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python
 
 import curses
-import curses.panel
 
 def main(screen):
-    curses.start_color()
     screen.border(0)
-    screen.addstr(0,0, 'Hello world')
+    screen.addstr('Main screen')
+    maxy, maxx = screen.getmaxyx()
+    cmd_win = screen.derwin(maxy-10,0)
+    cmd_win.border(0)
+    cmd_win.addstr('Command Window')
+    screen.refresh()
+    curses.echo()
+    cmd_win.addstr(1,1, '>> ')
+    txt = cmd_win.getstr(1,4)
 
-    sub_win = curses.newwin(10, 20, 30, 20)
-    sub_win.border(0)
-    sub_win.addstr(3,4, 'blah')
-    screen_panel = curses.panel.new_panel(screen)
-    sw_panel = curses.panel.new_panel(sub_win)
-    sw_panel.top()
-    curses.panel.update_panels()
-    curses.doupdate()
-    sub_win.getch()
-    sw_panel.hide()
-    screen.getch()
+    
+    #screen.getch()
     curses.endwin()
 
-
-if __name__ == '__main__':
+if __name__=='__main__':
     curses.wrapper(main)
+
